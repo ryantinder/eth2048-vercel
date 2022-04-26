18324cb8e5bed010415634cafb33848129450c78
ryantinder <ryantinder56@gmail.com>
2022-04-25 20:10:02 -0400

    Update Home.js

--- pages/Home.js
+++ pages/Home.js
@@ -88,7 +88,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Ceate Next App</title>
       </Head>
 
       <main>
